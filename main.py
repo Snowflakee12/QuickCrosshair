e5a73a8dba9c9a1308611d2641a039c312ee9bd9
Snowflakee12 <117774489+Snowflakee12@users.noreply.github.com>
2025-01-15 18:37:21 +0100

    Update main.py
    some uptade for the gravity change in the interface, and other things like zoom and data for some vehicule
    (There's not working perfectly still trying to figure out why)

--- main.py
+++ main.py
@@ -1,47 +1,169 @@
 import sys, math
-from PyQt5.QtWidgets import (
-    QApplication, QMainWindow, QWidget, QLabel, QComboBox,
-    QLineEdit, QPushButton, QGridLayout, QMessageBox
-)
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QComboBox, QLineEdit, QPushButton, QGridLayout, QMessageBox
 from PyQt5.QtCore import Qt, QPoint
 from PyQt5.QtGui import QPainter, QPen, QColor, QStandardItemModel, QStandardItem, QIcon
 
-projectiles = {
-    "25mm Bushmaster HE": {"velocity": 1000, "gravity_modifier": 2},
-    "30mm 2A72 HE": {"velocity": 900, "gravity_modifier": 2},
-    "30mm 2A42 HE": {"velocity": 900, "gravity_modifier": 2},
-    "DTB02 HE (ZBD04A)": {"velocity": 970, "gravity_modifier": 2},
-    "DTB02 HE (ZBL08)": {"velocity": 950, "gravity_modifier": 2},
-    "DTB02-105 HE": {"velocity": 1100, "gravity_modifier": 2},
-    "GPD-30 (BMP2M)": {"velocity": 230, "gravity_modifier": 1},
-    "ZU-23-2": {"velocity": 980, "gravity_modifier": 2},
-    "MK19": {"velocity": 230, "gravity_modifier": 1},
-    "Grenade launcher": {"velocity": 76, "gravity_modifier": 1},
-    "OG-15V frag": {"velocity": 290, "gravity_modifier": 1.2},
-    "2A70 frag": {"velocity": 355, "gravity_modifier": 2},
-    "ZIS-3 Fragmentation": {"velocity": 700, "gravity_modifier": 2},
-}
 
-projectiles_by_category = {
-    "Véhicule": [
-        "25mm Bushmaster HE",
-        "30mm 2A72 HE",
-        "30mm 2A42 HE",
-        "DTB02 HE (ZBD04A)",
-        "DTB02 HE (ZBL08)",
-        "GPD-30 (BMP2M)",
-        "2A70 frag",
-        "OG-15V frag",
-        "DTB02-105 HE",
-    ],
-    "Arme": [
-        "Grenade launcher",
-    ],
-    "Emplacement": [
-        "ZU-23-2",
-        "MK19",
-        "ZIS-3 Fragmentation",
-    ],
+vehicles = {
+    "IFV": {
+        "BMD-1M": {
+            "zoom_levels": [3.0],
+            "projectiles": {
+                "OG-15V frag": {"velocity": 290, "gravity_modifier": 1.2},
+            },
+        },
+        "BMD-4M & BMP-3M": {
+            "zoom_levels": [2.5],
+            "projectiles": {
+                "DTB02-100 HE": {"velocity": 355, "gravity_modifier": 2},
+                "3UOR6 HE": {"velocity": 900, "gravity_modifier": 2},
+            },
+        },
+        "BMP-1": {
+            "zoom_levels": [1.5],
+            "projectiles": {
+                "3UOR6 HE": {"velocity": 435, "gravity_modifier": 1.2},
+            },
+        },
+
+        "BMP-2 & BMP-2M": {
+            "zoom_levels": [1.5],
+            "projectiles": {
+                "3UOR6 HE": {"velocity": 900, "gravity_modifier": 2},
+            },
+        },
+        "BTR-82A": {
+            "zoom_levels": [2],
+            "projectiles": {
+                "3UOR6 HE": {"velocity": 900, "gravity_modifier": 2},
+            },
+        },
+        "ZBL-08": {
+            "zoom_levels": [2],
+            "projectiles": {
+                "DTB02-30 HEI-T": {"velocity": 950, "gravity_modifier": 2},
+            },
+        },         
+        "ZBD-04A": {
+            "zoom_levels": [2.5],
+            "projectiles": {
+                "DTB02-100 HE": {"velocity": 355, "gravity_modifier": 2},
+                "DTB02-30 HEI-T": {"velocity": 230, "gravity_modifier": 2},
+            },
+        },
+        "LAV-6": {
+            "zoom_levels": [1.5],
+            "projectiles": {
+                "MK210 HEI-T": {"velocity": 1000, "gravity_modifier": 2},
+            },
+        },
+        "LAV-25": {
+            "zoom_levels": [1],
+            "projectiles": {
+                "MK210 HEI-T": {"velocity": 1000, "gravity_modifier": 2},
+            },
+        },
+        "M2A3 et M7A3": {
+            "zoom_levels": [3],
+            "projectiles": {
+                "MK210 HEI-T": {"velocity": 1000, "gravity_modifier": 2},
+            },
+        },
+        "Aslav-25": {
+            "zoom_levels": [3],
+            "projectiles": {
+                "MK210 HEI-T": {"velocity": 1000, "gravity_modifier": 2},
+            },
+        },
+    },
+    "MBT & MGS": {
+        "T72B3": {
+            "zoom_levels": [4],
+            "projectiles": {
+                "OF26 Frag": {"velocity": 1100, "gravity_modifier": 2},
+                "3OF82 Frag": {"velocity": 1100, "gravity_modifier": 2},
+            },
+        },
+        "M1A2 & M1A1": {
+            "zoom_levels": [3],
+            "projectiles": {
+                "M830A1 HEAT": {"velocity": 1100, "gravity_modifier": 2},
+            },
+        },
+        "ZTZ99A": {
+            "zoom_levels": [4],
+            "projectiles": {
+                "DTB12-125 Frag": {"velocity": 1100, "gravity_modifier": 2},
+            },
+        },
+        "T-62": {
+            "zoom_levels": [3.5],
+            "projectiles": {
+                "OF-11 Frag": {"velocity": 1100, "gravity_modifier": 2},
+            },
+        },
+        "ZTD-05": {
+            "zoom_levels": [3.5],
+            "projectiles": {
+                "DTB02-105 Frag": {"velocity": 1100, "gravity_modifier": 2},
+            },
+        },        
+        "Sprut-SDM1": {
+            "zoom_levels": [4],
+            "projectiles": {
+                "3OF82 Frag": {"velocity": 1100, "gravity_modifier": 2},
+            },
+        },
+        "M1128 MGS": {
+            "zoom_levels": [3],
+            "projectiles": {
+                "M456A2 HEAT": {"velocity": 1100, "gravity_modifier": 2},
+            },
+        },          
+    },
+    
+    "APC": {
+        "AAVP": {
+            "zoom_levels": [1],
+            "projectiles": {
+                "MK19": {"velocity": 230, "gravity_modifier": 1},
+            },
+        },
+        "BRDM-UB32": {
+            "zoom_levels": [1],
+            "projectiles": {
+                "S-5": {"velocity": 300, "gravity_modifier": 1,"velocity_modifier":-50,"time":2},
+            },
+        },        
+    },
+    "Emplacement": {
+        "ZU-23-2": {
+            "zoom_levels": [1],
+            "projectiles": {
+                "ZU-23-2": {"velocity": 980, "gravity_modifier": 2},
+            },
+        },
+        "MK19": {
+            "zoom_levels": [1],
+            "projectiles": {
+                "MK19": {"velocity": 230, "gravity_modifier": 1},
+            },
+        },
+        "ZIS3": {
+            "zoom_levels": [3.7],
+            "projectiles": {
+                "ZIS-3 Frag": {"velocity": 700, "gravity_modifier": 2},
+            },
+        },        
+    },
+    "INF": {
+        "Grenade launcher": {
+            "zoom_levels": [1.0],
+            "projectiles": {
+                "Grenade launcher": {"velocity": 76, "gravity_modifier": 1},
+            },
+        },
+    },
 }
 
 class CrosshairWindow(QWidget):
@@ -71,10 +193,8 @@ class CrosshairWindow(QWidget):
         pen = QPen(QColor(255, 0, 0), 2)
         painter.setPen(pen)
         mid = self.rect().center()
-        painter.drawLine(mid.x() - self.crosshair_size, mid.y(),
-                         mid.x() + self.crosshair_size, mid.y())
-        painter.drawLine(mid.x(), mid.y() - self.crosshair_size,
-                         mid.x(), mid.y() + self.crosshair_size)
+        painter.drawLine(mid.x() - self.crosshair_size, mid.y(), mid.x() + self.crosshair_size, mid.y())
+        painter.drawLine(mid.x(), mid.y() - self.crosshair_size, mid.x(), mid.y() + self.crosshair_size)
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -86,77 +206,92 @@ class MainWindow(QMainWindow):
         centralWidget = QWidget()
         layout = QGridLayout()
 
-  
+        self.vehicleCombo = QComboBox()
         self.projectileCombo = QComboBox()
-        self.populateCombo()
-        self.projectileCombo.currentTextChanged.connect(self.changerProjectile)
+        self.zoomEdit = QLineEdit()
+
+        self.populateVehicleCombo()
+        self.vehicleCombo.currentTextChanged.connect(self.changerVehicule)
 
         self.vitesseEdit = QLineEdit()
         self.graviteEdit = QLineEdit()
         self.distanceEdit = QLineEdit()
         self.diffHauteurEdit = QLineEdit()
 
-    
-        self.zoomEdit = QLineEdit()
-        self.zoomEdit.setText("1")
-
-        self.calculerBtn = QPushButton("Calculer le décalage")
+        self.calculerBtn = QPushButton("Calculer le d\u00e9calage")
         self.calculerBtn.clicked.connect(self.calculerDecalage)
 
-        self.toggleCrosshairBtn = QPushButton("Désactiver le crosshair")
+        self.toggleCrosshairBtn = QPushButton("D\u00e9sactiver le crosshair")
         self.toggleCrosshairBtn.setCheckable(True)
-        self.toggleCrosshairBtn.setChecked(True)  
+        self.toggleCrosshairBtn.setChecked(True)
         self.toggleCrosshairBtn.clicked.connect(self.toggleCrosshair)
 
-
-        layout.addWidget(QLabel("Projectile:"), 0, 0)
-        layout.addWidget(self.projectileCombo, 0, 1)
-        layout.addWidget(QLabel("Vitesse (m/s):"), 1, 0)
-        layout.addWidget(self.vitesseEdit, 1, 1)
-        layout.addWidget(QLabel("Gravité (m/s²):"), 2, 0)
-        layout.addWidget(self.graviteEdit, 2, 1)
-        layout.addWidget(QLabel("Distance (m):"), 3, 0)
-        layout.addWidget(self.distanceEdit, 3, 1)
-        layout.addWidget(QLabel("Diff. d'altitude (m):"), 4, 0)
-        layout.addWidget(self.diffHauteurEdit, 4, 1)
-        layout.addWidget(QLabel("Zoom:"), 5, 0)
-        layout.addWidget(self.zoomEdit, 5, 1)
-        layout.addWidget(self.calculerBtn, 6, 0, 1, 2)
-        layout.addWidget(self.toggleCrosshairBtn, 7, 0, 1, 2)
+        layout.addWidget(QLabel("Type de v\u00e9hicule:"), 0, 0)
+        layout.addWidget(self.vehicleCombo, 0, 1)
+        layout.addWidget(QLabel("Projectile:"), 1, 0)
+        layout.addWidget(self.projectileCombo, 1, 1)
+        layout.addWidget(QLabel("Vitesse (m/s):"), 2, 0)
+        layout.addWidget(self.vitesseEdit, 2, 1)
+        layout.addWidget(QLabel("Gravit\u00e9 (m/s\u00b2):"), 3, 0)
+        layout.addWidget(self.graviteEdit, 3, 1)
+        layout.addWidget(QLabel("Distance (m):"), 4, 0)
+        layout.addWidget(self.distanceEdit, 4, 1)
+        layout.addWidget(QLabel("Diff. d'altitude (m):"), 5, 0)
+        layout.addWidget(self.diffHauteurEdit, 5, 1)
+        layout.addWidget(QLabel("Zoom:"), 6, 0)
+        layout.addWidget(self.zoomEdit, 6, 1)
+        layout.addWidget(self.calculerBtn, 7, 0, 1, 2)
+        layout.addWidget(self.toggleCrosshairBtn, 8, 0, 1, 2)
 
         centralWidget.setLayout(layout)
         self.setCentralWidget(centralWidget)
+        self.changerVehicule(self.vehicleCombo.currentText())
 
-        self.changerProjectile(self.projectileCombo.currentText())
-
-    def populateCombo(self):
+    def populateVehicleCombo(self):
         model = QStandardItemModel()
-        for category, items in projectiles_by_category.items():
+        for category, vehicle_data in vehicles.items():
             header = QStandardItem(category + ":")
             header.setFlags(Qt.NoItemFlags)
             model.appendRow(header)
-            for item in items:
-                projectile_item = QStandardItem("   " + item)
-                model.appendRow(projectile_item)
-        self.projectileCombo.setModel(model)
-        for i in range(model.rowCount()):
-            text = model.item(i).text().strip()
-            if text in projectiles:
-                self.projectileCombo.setCurrentIndex(i)
-                break
+            for vehicle in vehicle_data:
+                vehicle_item = QStandardItem("   " + vehicle)
+                model.appendRow(vehicle_item)
+        self.vehicleCombo.setModel(model)
 
-    def changerProjectile(self, text):
+    def changerVehicule(self, text):
         text = text.strip()
         if text.endswith(":"):
             return
         if text.startswith("   "):
             text = text.strip()
-        if text not in projectiles:
+        for category, vehicle_data in vehicles.items():
+            if text in vehicle_data:
+                details = vehicle_data[text]
+                self.zoomEdit.setText(", ".join(map(str, details.get("zoom_levels", []))))
+                self.projectileCombo.clear()
+                if details.get("projectiles"):
+                    self.projectileCombo.addItems(details["projectiles"])
+                    self.projectileCombo.currentTextChanged.connect(self.changerProjectile)
+                    if self.projectileCombo.count() > 0:
+                        self.changerProjectile(self.projectileCombo.itemText(0))
+                else:
+                    QMessageBox.warning(self, "Erreur", "Aucun projectile disponible pour ce v\u00e9hicule.")
+                break
+
+    def changerProjectile(self, text):
+        text = text.strip()
+        if not text:
             return
-        projectile = projectiles[text]
-        self.vitesseEdit.setText(str(projectile["velocity"]))
-        gravite_base = 9.81
-        self.graviteEdit.setText(str(projectile["gravity_modifier"] * gravite_base))
+        current_vehicle = self.vehicleCombo.currentText().strip()
+        for category, vehicle_data in vehicles.items():
+            for vehicle, details in vehicle_data.items():
+                if current_vehicle == vehicle:
+                    projectile_data = details["projectiles"].get(text)
+                    if projectile_data:
+                        self.vitesseEdit.setText(str(projectile_data["velocity"]))
+                        gravite_base = 9.78
+                        self.graviteEdit.setText(str(projectile_data["gravity_modifier"] * gravite_base))
+                    return
 
     def calculerDecalage(self):
         try:
@@ -164,7 +299,10 @@ class MainWindow(QMainWindow):
             g = float(self.graviteEdit.text())
             d = float(self.distanceEdit.text())
             dy = float(self.diffHauteurEdit.text())
+            if v <= 0 or d <= 0:
+                raise ValueError("Les valeurs de vitesse et de distance doivent \u00eatre positives.")
         except ValueError:
+            QMessageBox.warning(self, "Erreur", "Veuillez entrer des valeurs valides et positives.")
             return
 
         try:
@@ -173,29 +311,34 @@ class MainWindow(QMainWindow):
             C = A + dy
             discriminant = B**2 - 4 * A * C
             if discriminant < 0:
+                QMessageBox.warning(self, "Erreur", "Le calcul du d\u00e9calage est impossible.")
+                return
+            if A == 0:
+                QMessageBox.warning(self, "Erreur", "Le calcul est impossible car A est nul.")
                 return
             u = (-B - math.sqrt(discriminant)) / (2 * A)
             theta = math.atan(u)
         except Exception as e:
-            print("Erreur dans le calcul:", e)
+            QMessageBox.warning(self, "Erreur", f"Erreur dans le calcul : {e}")
             return
 
         try:
             zoom_factor = float(self.zoomEdit.text())
+            if zoom_factor <= 0:
+                zoom_factor = 1.0
         except ValueError:
             zoom_factor = 1.0
 
-        FOV_v = 90
+        FOV_v = 90 / zoom_factor
         screen_height = QApplication.primaryScreen().geometry().height()
         pixels_par_deg = screen_height / FOV_v
-
-        decalage_pixels = math.degrees(theta) * pixels_par_deg * zoom_factor
+        decalage_pixels = math.degrees(theta) * pixels_par_deg
         self.crosshairWindow.setOffset(0, decalage_pixels)
 
     def toggleCrosshair(self, checked):
         if checked:
             self.crosshairWindow.show()
-            self.toggleCrosshairBtn.setText("Désactiver le crosshair")
+            self.toggleCrosshairBtn.setText("D\u00e9sactiver le crosshair")
         else:
             self.crosshairWindow.hide()
             self.toggleCrosshairBtn.setText("Activer le crosshair")
